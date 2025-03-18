8b4eb51a28bffec7b8ee07bc94806260fd4fd22b
CHOI-HYOJEONG <oreoz00@o.cnu.ac.kr>
2025-03-18 16:17:42 +0900

    Update priority_queue.py

--- priority_queue.py
+++ priority_queue.py
@@ -10,11 +10,11 @@ class PriorityQueue:
   def pop(self):
     if len(self.heap) > 1:
       self._swap(0, len(self.heap) - 1)
-      priority, item = self.heap.pop()
+      -, item = self.heap.pop()
       self._sift_down(0)
       return item
     elif len(self.heap) == 1:
-      priority, item = self.heap.pop()
+      -, item = self.heap.pop()
       return item
     else:
       return None
@@ -34,11 +34,11 @@ class PriorityQueue:
       right_child_index = 2 * index + 2
       smallest = index
 
-      if left_child_index < len(self.heap) and \
+      if left_child_index > len(self.heap) and \
         self.heap[left_child_index][0] > self.heap[smallest][0]:
         smallest = left_child_index
 
-      if right_child_index < len(self.heap) and \
+      if right_child_index > len(self.heap) and \
         self.heap[right_child_index][0] > self.heap[smallest][0]:
         smallest = right_child_index
 
